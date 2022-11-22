ea991d602ac7ce028e4f96db94b02840d7d6ba68
maciej <mkrywult88@gmail.com>
2022-11-22 18:25:40 +0100

    echo response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,18 @@
 from django.http import JsonResponse
+import json
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "API response"})
+    print(request.GET)
+    print(request.POST)
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
 
