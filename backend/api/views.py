d9f3e41a2cfe596dcfa17f86dc473efa4f5f872f
maciej <mkrywult88@gmail.com>
2022-11-23 17:04:14 +0100

    DRF API view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,18 @@
-import json
-from django.http import JsonResponse, HttpResponse
 from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from products.models import Product
+
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
         data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-    return JsonResponse(data)
+    return Response(data)
 #        json_data_str = json.dumps(data)
 #    return HttpResponse(json_data_str, headers={"content-type": "application/json"})
 
