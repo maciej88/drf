e681552d100efd28e897f547806a8b9718e26ffa
maciej <mkrywult88@gmail.com>
2022-11-23 16:55:29 +0100

    try to data dumps

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,12 @@
 import json
-from django.http import JsonResponse
-
+from django.http import JsonResponse, HttpResponse
+from django.forms.models import model_to_dict
 from products.models import Product
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-    return JsonResponse(data)
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+        json_data_str = json.dumps(data)
+    return HttpResponse(json_data_str, headers={"content-type": "application/json"})
 
