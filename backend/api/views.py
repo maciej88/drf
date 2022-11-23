77214e8fba448cc5979b4fce76812caff04e894e
maciej <mkrywult88@gmail.com>
2022-11-23 17:08:52 +0100

    sale_price in views add

--- backend/api/views.py
+++ backend/api/views.py
@@ -11,7 +11,7 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+        data = model_to_dict(model_data, fields=['id', 'title', 'price', 'sale_price'])
     return Response(data)
 #        json_data_str = json.dumps(data)
 #    return HttpResponse(json_data_str, headers={"content-type": "application/json"})
