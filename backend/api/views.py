ba622b9a17f60aa9edfa8cfa7f09834c0aba4417
maciej <mkrywult88@gmail.com>
2022-11-23 17:28:49 +0100

    ImportError: attempted relative import beyond top-level package

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,8 @@
-from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from ..products.serializers import ProductSerializer
-from ..products.models import Product
+from products.models import Product
+from products.serializers import ProductSerializer
 
 @api_view(['GET'])
 def api_home(request, *args, **kwargs):
