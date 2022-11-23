349fb3f5f37291ae1e24e13b59043c394c8fdb3b
maciej <mkrywult88@gmail.com>
2022-11-23 17:14:17 +0100

    simlarity between forms and serializers

---
+++ backend/products/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Product
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        models = Product
+        fields = [
+            'title',
+            'content',
+            'price'
+        ]
