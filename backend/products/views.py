94b4fb7645be085b615c398a3d9660459fb43c46
maciej <mkrywult88@gmail.com>
2022-11-24 18:10:03 +0100

    generic view and create

--- backend/products/views.py
+++ backend/products/views.py
@@ -6,3 +6,10 @@ from .serializers import ProductSerializer
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+product_detail_view = ProductDetailAPIView.as_view()
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+product_create_view = ProductCreateAPIView.as_view()
