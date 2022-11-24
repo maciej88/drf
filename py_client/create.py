bacee3c2761228a1eecc28d0a2fb7f7f38b194b5
maciej <mkrywult88@gmail.com>
2022-11-24 18:13:25 +0100

    create.py with data

--- py_client/create.py
+++ py_client/create.py
@@ -2,6 +2,10 @@ import requests
 
 endpoint = 'http://localhost:8000/api/products/'
 
-get_response = requests.post(endpoint)
+get_response = requests.post(endpoint, json=data)
 
+data = {
+    "title": "done!",
+    "price": 32.99
+}
 print(get_response.text)
