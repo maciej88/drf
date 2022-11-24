b19036d9343b7d4b63fcb0a5369d8c762868de24
maciej <mkrywult88@gmail.com>
2022-11-24 18:43:09 +0100

    alt view

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,8 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from django.shortcuts import get_object_or_404
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -16,10 +20,34 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         # serializer.save(user)
         pass
 
-product_list_create_view = ProductCreateAPIView.as_view()
+product_list_create_view = ProductListCreateAPIView.as_view()
 
 class ProductListAPIView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 product_list_view = ProductDetailAPIView.as_view()
+
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if method == "GET":
+        if pk is not None:
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+
+    if method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True)
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+        return Response(serializer.data)
+    return Response({"inva;od": "wrong data"}, status=400)
