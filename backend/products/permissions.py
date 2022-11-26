96d825fd2b968b6a8c87dbafbfc3b964385949e0
maciej <mkrywult88@gmail.com>
2022-11-26 14:31:25 +0100

    permissions change

--- backend/products/permissions.py
+++ backend/products/permissions.py
@@ -4,7 +4,15 @@ class IsStaffEditorPermission(permissions.DjangoModelPermissions):
     def has_permission(self, request, view):
         user = request.user
         if user.is_staff:
-            return True
+            if user.has_perm("products.add_product"): # "app_name.verb_model_name
+                return True
+            if user.has_perm("products.delete_product"):
+                return True
+            if user.has_perm("products.change_product"):
+                return True
+            if user.has_perm("products.view_product"):
+                return True
+            return False
         return False
 
 #    def has_object_permission(self, request, view, obj):
