367373b5a366b664a0cc53b1006e355cf5d4eef7
maciej <mkrywult88@gmail.com>
2022-11-26 14:38:28 +0100

    permissions change

--- backend/products/permissions.py
+++ backend/products/permissions.py
@@ -2,18 +2,23 @@ from rest_framework import permissions
 
 class IsStaffEditorPermission(permissions.DjangoModelPermissions):
     def has_permission(self, request, view):
-        user = request.user
-        if user.is_staff:
-            if user.has_perm("products.add_product"): # "app_name.verb_model_name
-                return True
-            if user.has_perm("products.delete_product"):
-                return True
-            if user.has_perm("products.change_product"):
-                return True
-            if user.has_perm("products.view_product"):
-                return True
+        if not request.user.is_staff:
             return False
-        return False
+        return super().has_permission(request, view)
+
+    # def has_permission(self, request, view):
+    #     user = request.user
+#        if user.is_staff:
+#            if user.has_perm("products.add_product"): # "app_name.verb_model_name
+#                return True
+#             if user.has_perm("products.delete_product"):
+#                 return True
+#             if user.has_perm("products.change_product"):
+#                 return True
+#             if user.has_perm("products.view_product"):
+#                 return True
+#             return False
+#         return False
 
 #    def has_object_permission(self, request, view, obj):
 #        return obj.owner == request.user
