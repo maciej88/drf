f8dadff5b3e29cd73d6a93b406ffbb46821aeb1b
maciej <mkrywult88@gmail.com>
2022-11-29 17:55:41 +0100

    ProductGenericViewSets

--- backend/products/viewsets.py
+++ backend/products/viewsets.py
@@ -1,9 +1,29 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 
 from .models import Product
 from .serializers import ProductSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
+    '''
+    get -> list -> queryset
+    get -> retrieve -> Product Instance Detail View
+    post -> create -> new instance
+    put -> update
+    patch -> partial update
+    delete -> destroy
+    '''
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+class ProductGenericViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet):
+    '''
+    get -> list -> queryset
+    get -> retrieve -> Product Instance Detail View
+    '''
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'pk'
