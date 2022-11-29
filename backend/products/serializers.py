c1e739ce00b543b66c98cdacba99428633b6918e
maciej <mkrywult88@gmail.com>
2022-11-29 18:41:50 +0100

    detail link as url

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -4,11 +4,16 @@ from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     # my_discount = serializers.SerializerMethodField(read_only=True)
-    url = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field= 'pk'
+    )
     class Meta:
         model = Product
         fields = [
             'url',
+            'edit_url',
             'pk',
             'title',
             'content',
@@ -16,12 +21,12 @@ class ProductSerializer(serializers.ModelSerializer):
             'sale_price'
         ]
 
-    def get_url(self, obj):
+    def get_edit_url(self, obj):
         #return f"/api/products/{obj.pk}/"
         request = self.context.get('request')
         if request is None:
             return None
-        return reverse("product-detail", kwargs={"pk": obj.pk}, request=request)
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
 
     # def get_my_discount(self, obj):
     #     if not hasattr(obj, 'id'):
