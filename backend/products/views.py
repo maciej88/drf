fae444e0a4b0bd579455c1d4ec096e2a08504c31
maciej <mkrywult88@gmail.com>
2022-11-30 19:51:39 +0100

    exaple clear

--- backend/products/views.py
+++ backend/products/views.py
@@ -74,7 +74,6 @@ class ProductListCreateAPIView(
 
     def perform_create(self, serializer):
         # serializer.save(user)
-        email = serializer.validated_data.pop('email')
         title = serializer.validated_data.get('title')
         content = serializer.validated_data.get('content') or None
         if content is None:
