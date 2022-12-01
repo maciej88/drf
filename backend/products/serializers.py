7736c99cc457fe05e1a1f5ef0038185338f42e51
maciej <mkrywult88@gmail.com>
2022-12-01 20:23:39 +0100

    title validation comment

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -22,11 +22,11 @@ class ProductSerializer(serializers.ModelSerializer):
             'sale_price'
         ]
 
-    def validate_title(self, value):
-        qs = Product.objects.filter(title__iexact=value)
-        if qs.exists():
-            raise serializers.ValidationError(f"{value} is already a product name")
-        return value
+    # def validate_title(self, value):
+    #     qs = Product.objects.filter(title__iexact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f"{value} is already a product name")
+    #     return value
 
     # def create(self, validated_data):
     #     #return Product.objects.create(**validated_data)
