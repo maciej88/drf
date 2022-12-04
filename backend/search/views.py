5e8f15f1d4908c02bd65d05660ca5522adfb77e9
maciej <mkrywult88@gmail.com>
2022-12-04 18:36:16 +0100

    search engine

--- backend/search/views.py
+++ backend/search/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from products.models import Product
+from products.serializers import ProductSerializer
+
+class SearchListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        q = self.request.GET.get('q')
+        user = None
+        if self.request.user.is_authenticated:
+            user = self.request.user
+        results = qs.search()
+        return results
