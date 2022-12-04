95e0b94196237f68660be0731c090c1d3cbd1564
maciej <mkrywult88@gmail.com>
2022-12-04 17:09:16 +0100

    public in models

--- backend/products/models.py
+++ backend/products/models.py
@@ -4,11 +4,14 @@ from django.conf import settings
 
 User = settings.AUTH_USER_MODEL
 
+
+
 class Product(models.Model):
     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=123)
     content = models.TextField(blank=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, default= 99.99)
+    public = models.BooleanField(default=True)
 
     @property
     def sale_price(self):
