78c1c57b23f5ef73d4005925a0114f91f22f757a
maciej <mkrywult88@gmail.com>
2022-12-04 16:40:21 +0100

    pagination in settings.py

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -128,6 +128,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 10
+}
 # REST_FRAMEWORK = {
 #     "DEFAULT_AUTHENTICATION_CLASSES": [
 #         "rest_framework.authentication.SessionAuthentication",
