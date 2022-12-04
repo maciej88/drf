f8e5e52698beebcc3f945df1dbec3c97922c2bcd
maciej <mkrywult88@gmail.com>
2022-12-04 17:32:00 +0100

    query search in models

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 from django.conf import settings
+from django.db import Q
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL
 
+class ProductQuerySet(models.QuerySet):
+    def is_public(self):
+        return self.filter(public=True)
 
+    def search(self, query, user=None):
+        lookup = Q(title__icontains=query) | Q(content__icontains=query)
+        qs = self.filter(lookup)
+        if user is not None:
+            qs = qs.filter(user=user)
+        return qs
+
+class ProductManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return ProductQuerySet(self.model, using=self._db)
+    def search (self, query, user=None):
+        return self.get_queryset().is_public().search(query, user=user)
 
 class Product(models.Model):
     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
