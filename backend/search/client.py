608ffbc6913e1fe5727a00e57bf82917284127ee
maciej <mkrywult88@gmail.com>
2022-12-07 13:30:45 +0100

    perform search

--- backend/search/client.py
+++ backend/search/client.py
@@ -7,3 +7,8 @@ def get_index(index_name='cfe_Product'):
     client = get_client()
     index = client.init_index(index_name)
     return index
+
+def perform_search(query, **kwargs):
+    index = get_index()
+    results = index.search(query)
+    return results
