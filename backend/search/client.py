c7dff71509f461a5304622ce51a7628c0d9a0661
maciej <mkrywult88@gmail.com>
2022-12-07 13:29:05 +0100

    get index

--- backend/search/client.py
+++ backend/search/client.py
@@ -2,3 +2,8 @@ from algoliasearch_django import algolia_engine
 
 def get_client():
     return algolia_engine.client
+
+def get_index(index_name='cfe_Product'):
+    client = get_client()
+    index = client.init_index(index_name)
+    return index
